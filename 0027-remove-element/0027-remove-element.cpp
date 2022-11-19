bac949fd256121294c9825e848067cc2826f3507
Shubham Semwal <50329164+shubhamsemwal11@users.noreply.github.com>
2022-11-19 12:20:04 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (73.45%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int index = 0;
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            
+            if(nums[i] != val) {
+                
+                swap(nums[i], nums[index]);
+                index++;
+            }
+        }
+        
+        return index;
+    }
+};
